bce689bc9fe99e4b496ef7d366822d73cb0a2d7f
goncalo.henrique <gonriq01@gmail.com>
2026-05-16 14:40:48 -0300

    feat: arrumando conexao com banco

--- php/conexao.php
+++ php/conexao.php
@@ -3,7 +3,7 @@
 $servidor = "127.0.0.1"; // Usem localhost:3306 (no meu contexto precisei utilizar o IP pra funcionar)
 $usuario  = "root";
 $senha    = "";
-$nome_banco = "Ecomercy";
+$nome_banco = "konnect";
 
 // O driver mysqli já sabe que deve procurar a porta 3306 por padrão
 $conexao = new mysqli($servidor, $usuario, $senha, $nome_banco);
