e87da839d5bdf795183e599bd56050b247ed4ef5
goncalo.henrique <gonriq01@gmail.com>
2026-05-16 14:32:18 -0300

    feat: retirando senha do commit

--- php/conexao.php
+++ php/conexao.php
@@ -2,7 +2,7 @@
 // Variáveis de conexão com o Banco de Dados
 $servidor = "127.0.0.1"; // Usem localhost:3306 (no meu contexto precisei utilizar o IP pra funcionar)
 $usuario  = "root";
-$senha    = "180207";
+$senha    = "";
 $nome_banco = "Ecomercy";
 
 // O driver mysqli já sabe que deve procurar a porta 3306 por padrão
